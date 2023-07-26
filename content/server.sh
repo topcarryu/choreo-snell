055bc772ec8a316ff42364ee2460df003f96cbfc
topcarryu <107200176+topcarryu@users.noreply.github.com>
2023-07-26 22:28:24 +0800

    .

--- content/server.sh
+++ content/server.sh
@@ -3,8 +3,8 @@
 set -e
 exec 2>&1
 
-PASS=${PASS:-'abcdefg'}
-PATH=${PATH:-'user'}
+PASSWD=${PASSWD:-'abcdefg'}
+WSPATH=${WSPATH:-'user'}
 
 generate_config() {
   cat > /tmp/config.json << EOF
@@ -17,9 +17,9 @@ generate_config() {
         "server": "0.0.0.0",
         "server_port": 58128,
         "method": "chacha20-ietf-poly1305",
-        "password": "${PASS}",
+        "password": "${PASSWD}",
         "plugin": "/usr/src/app/plugin",
-        "plugin_opts": "server;path=/${PATH};mode=websocket",
+        "plugin_opts": "server;path=/${WSPATH};mode=websocket",
         "plugin_mode": "tcp_and_udp",
         "timeout": 7200
       }
